9d42d2b85c8511a9ceb4aedd061cabe5cdb28de6
Md. Sindabad <pavel.sindabad@gmail.com>
2021-10-15 18:26:23 +0600

    products added to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import useProducts from '../../hooks/useProducts';
+import Error from '../../utilities/Error';
+import Loader from '../../utilities/Loader';
+import Product from '../Product/Product';
 import ImageSlider from '../Slider/ImageSlider';
 
 const Home = () => {
+    const { isLoading, products, error } = useProducts();
     return (
-        <div>
+        <div className="pb-3">
             <ImageSlider />
+            <h2 className="ms-3">Our Top Products</h2>
+            <Container className="d-flex justify-content-center">
+                {
+                    error ?
+                        <Error /> :
+                        isLoading ? <Loader /> :
+                            <Row>
+                                {products.slice(0, 6).map(product => {
+                                    return <Product key={product.id} data={product} />
+                                })}
+                            </Row>
+                }
+            </Container>
         </div>
     )
 }
--- src/components/Slider/ImageSlider.js
+++ src/components/Slider/ImageSlider.js
@@ -7,18 +7,21 @@ const ImageSlider = () => {
         <Carousel>
             <Carousel.Item className="slider">
                 <img
+                    className="d-block w-100"
                     src="https://i.ibb.co/9GdDHKD/3255317.jpg"
                     alt="First slide"
                 />
             </Carousel.Item>
             <Carousel.Item className="slider">
                 <img
+                    className="d-block w-100"
                     src="https://i.ibb.co/Mc9SW6R/2741840.jpg"
                     alt="Second slide"
                 />
             </Carousel.Item>
             <Carousel.Item className="slider">
                 <img
+                    className="d-block w-100"
                     src="https://i.ibb.co/ckhmpsS/Store-with-credit-card-gift-boxes-buyers-vector-illustration-Purchase-sale-e-commerce-Shopping-conce.jpg"
                     alt="Third slide"
                 />
