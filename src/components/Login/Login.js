430eceedcf707b8840af0279863130872f7f8aaf
Md. Sindabad <pavel.sindabad@gmail.com>
2021-10-16 14:04:01 +0600

    form style added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import { FaGoogle } from 'react-icons/fa';
+import { FaGoogle, FaFacebookSquare } from 'react-icons/fa';
+import './login.css';
 
 const Login = () => {
     const handleFormSubmit = (e) => {
@@ -11,8 +12,8 @@ const Login = () => {
         <Container>
             <h1 className="text-center my-3">Login</h1>
             <Row className="d-flex justify-content-center">
-                <Col xs={10} md={6}>
-                    <Form onSubmit={handleFormSubmit}>
+                <Col xs={12} md={6}>
+                    <Form onSubmit={handleFormSubmit} className="form">
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
@@ -21,11 +22,14 @@ const Login = () => {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Enter password" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+                        <Button className="form-control" variant="success" type="submit">
                             Login
                         </Button>
-                        <Button className="d-flex align-items-center justify-content-center my-3 form-control" variant="dark" type="submit">
+                        <Button className="d-flex align-items-center justify-content-center my-2 form-control" variant="dark" type="submit">
                             <FaGoogle className="me-2" /> Google
+                        </Button>
+                        <Button className="d-flex align-items-center justify-content-center my-2 form-control" variant="primary" type="submit">
+                            <FaFacebookSquare className="me-2" /> Facebook
                         </Button>
                     </Form>
                 </Col>
